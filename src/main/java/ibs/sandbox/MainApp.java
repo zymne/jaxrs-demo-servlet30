dcab08febc5a15ce1afbcb1f7027c8cb949a7c27
Ключников Евгений Владимирович <EKlyuchnikov@ibs.root.ibscorp.ru>
2019-09-02 20:37:07 +0300

    add JNDIRealm

--- src/main/java/ibs/sandbox/MainApp.java
+++ src/main/java/ibs/sandbox/MainApp.java
@@ -11,7 +11,6 @@ import org.apache.tomcat.util.descriptor.web.*;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.naming.NamingException;
-import javax.servlet.ServletException;
 import java.io.File;
 
 
@@ -58,13 +57,57 @@ public class MainApp extends ResourceConfig {
         restServiceContext.getPipeline().addValve(new FormAuthenticator());
 
         JNDIRealm realm = new JNDIRealm();
-        //realm.setConnectionURL();
+        realm.setConnectionURL("ldap://esk-dc3.eispd.dkz.cloud:389");
+        //realm.setUserBase("dc=cbr,dc=ru");
+        //realm.setUserSearch("(samAccountName={0})");
+        realm.setUserPattern("CN={0},OU=Accounts,OU=TechUsersZPE,OU=ZPE,OU=eispd,DC=portal,DC=cbr,DC=ru");
+        realm.setUserRoleName("memberOf");
+        realm.setRoleBase("DC=cbr,DC=ru");
+        realm.setRoleName("cn");
+        realm.setRoleSearch("(uniqueMember={0})");
 
         //loginConf - для данного контекста пропишем механизмы аутентификации
         LoginConfig loginConfig = new LoginConfig();
         loginConfig.setAuthMethod("FORM");
+        loginConfig.setLoginPage("/login.html");
+        loginConfig.setErrorPage("/error.html");
         loginConfig.setRealmName("ldap");
         restServiceContext.setLoginConfig(loginConfig);
+        //TODO: нужен ли? или засетить имя реалма достаточно?
+        restServiceContext.setRealm(realm);
+
+        //web-resource-collection
+        SecurityCollection secCollection = new SecurityCollection();
+        secCollection.addPatternDecoded("/api/admin/*");
+
+        //authConstraint
+        SecurityConstraint secConstraint = new SecurityConstraint();
+        secConstraint.addCollection(secCollection);
+        secConstraint.addAuthRole("siteAdmin");
+        restServiceContext.addConstraint(secConstraint);
+
+        //Сервлет для Jersey (dispatcher)
+        Wrapper servlet = restServiceContext.createWrapper();
+        servlet.setName("jerseyContainer");
+        servlet.setServletClass("org.glassfish.jersey.servlet.ServletContainer");
+        servlet.addInitParameter("javax.ws.rs.Application", "ibs.sandbox.MainApp");
+        servlet.setLoadOnStartup(1);
+
+        //Default Servlet for serving static content
+        Wrapper defaultServlet = restServiceContext.createWrapper();
+        defaultServlet.setName("default");
+        defaultServlet.setServletClass("org.apache.catalina.servlets.DefaultServlet");
+        defaultServlet.addInitParameter("debug", "0");
+        defaultServlet.addInitParameter("listings", "false");
+        defaultServlet.setLoadOnStartup(1);
+
+
+        restServiceContext.addChild(servlet);
+        restServiceContext.addServletMappingDecoded("/api/*", "jerseyContainer");
+
+        restServiceContext.addChild(defaultServlet);
+        restServiceContext.addServletMappingDecoded("*.html", "default");
+
     }
 
     private Tomcat setup() {
@@ -87,6 +130,8 @@ public class MainApp extends ResourceConfig {
 
         //StandardHost host = new StandardHost();
 
+        createRestServiceApp();
+
         UserDatabaseRealm realm = new UserDatabaseRealm();
         realm.setResourceName("jndiUserDatabase");
         realm.setRealmPath("/config/tomcat-users.xml");
@@ -95,7 +140,7 @@ public class MainApp extends ResourceConfig {
         memoryRealm.setDomain("memory2");
         memoryRealm.setPathname("/config/tomcat-users.xml");
 
-        tomcat.getHost().setRealm(memoryRealm);
+        //tomcat.getHost().setRealm(memoryRealm);
 
         Context appContext = tomcat.addContext("/app", new File(docBase).getAbsolutePath());
         appContext.getPipeline().addValve(new BasicAuthenticator());
@@ -105,24 +150,6 @@ public class MainApp extends ResourceConfig {
         loginConfig.setAuthMethod("BASIC");
         loginConfig.setRealmName("memory2");
         appContext.setLoginConfig(loginConfig);
-
-        //web-resource-collection
-        SecurityCollection secCollection = new SecurityCollection();
-        secCollection.addPatternDecoded("/api/admin/*");
-
-        //authConstraint
-        SecurityConstraint secConstraint = new SecurityConstraint();
-        secConstraint.addCollection(secCollection);
-        secConstraint.addAuthRole("siteAdmin");
-        appContext.addConstraint(secConstraint);
-
-        appContext.addSecurityRole("siteAdmin");
-
-        Wrapper servlet = appContext.createWrapper();
-        servlet.setName("ibs.sandbox.MainApp");
-        servlet.setServletClass("org.glassfish.jersey.servlet.ServletContainer");
-        servlet.addInitParameter("javax.ws.rs.Application", "ibs.sandbox.MainApp");
-        servlet.setLoadOnStartup(1);
 
         Wrapper defaultServlet = appContext.createWrapper();
         defaultServlet.setName("default");
@@ -135,20 +162,17 @@ public class MainApp extends ResourceConfig {
         testServlet.setName("test-servlet");
         testServlet.setServletClass("ibs.sandbox.servlet.TestPage");
 
-
         appContext.addChild(testServlet);
         appContext.addServletMappingDecoded("/test/*", "test-servlet");
 
         appContext.addChild(defaultServlet);
         appContext.addServletMappingDecoded("*.html", "default");
 
-        appContext.addChild(servlet);
-        appContext.addServletMappingDecoded("/api/*", "ibs.sandbox.MainApp");
-
         return tomcat;
     }
 
-    public static void main(String[] args) throws ServletException, NamingException {
+    public static void main(String[] args) {
+
         Tomcat tomcat = new MainApp().setup();
         try {
             tomcat.start();
